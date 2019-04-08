d0c14c508bf6560290b27c65f290c0449987ab91
Coci0alex <AlexanderCS1@hotmail.com>
2019-04-08 11:30:51 +0200

    Update passwords_controller.rb

--- app/controllers/devise_token_auth/passwords_controller.rb
+++ app/controllers/devise_token_auth/passwords_controller.rb
@@ -5,6 +5,11 @@ module DeviseTokenAuth
     before_action :set_user_by_token, only: [:update]
     before_action :validate_redirect_url_param, only: [:create, :edit]
     skip_after_action :update_auth_header, only: [:create, :edit]
+    before_action: :disable_cookies
+    
+    def disable_cookies
+     request.session_options[:skip] = true
+    end
 
     # this action is responsible for generating password reset tokens and
     # sending emails
